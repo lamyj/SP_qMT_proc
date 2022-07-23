e86a7ff9114b6c1d406aaf4eac8546ba4827813d
Julien Lamy <lamy@unistra.fr>
2022-07-23 09:57:59 +0200

    Remove GSLFunction

--- jsp_qmt/super_lorentzian.cpp
+++ jsp_qmt/super_lorentzian.cpp
@@ -6,15 +6,14 @@
 #include <gsl/gsl_integration.h>
 #include <xtensor/xarray.hpp>
 
-#include "GSLFunction.h"
-
 thread_local gsl_integration_workspace * workspace =
     gsl_integration_workspace_alloc(1000);
 
 double
-super_lorentzian_integrand(double x, std::pair<double, double> const & params)
+super_lorentzian_integrand(double x, void * data)
 {
-    auto & [T2r, delta_f] = params;
+    auto & args = *reinterpret_cast<std::pair<double, double> *>(data);
+    auto & [T2r, delta_f] = args;
     return 
         T2r/std::fabs((3*std::pow(x, 2)-1))
         * std::exp(-2*std::pow(2*M_PI*delta_f*T2r / (3*std::pow(x, 2)-1), 2));
@@ -23,8 +22,7 @@ super_lorentzian_integrand(double x, std::pair<double, double> const & params)
 double super_lorentzian(double T2r, double delta_f)
 {
     auto data = std::make_pair(T2r, delta_f);
-    GSLFunction<decltype(super_lorentzian_integrand), decltype(data)> function(
-        super_lorentzian_integrand, data);
+    gsl_function function{super_lorentzian_integrand, &data};
     double result, abserr;
     gsl_integration_qags(
         &function, 0., 1., 1e-6, 1e-6, 50, workspace, &result, &abserr);
@@ -35,8 +33,7 @@ xt::xarray<double>
 super_lorentzian(double T2r, xt::xarray<double> const & delta_f)
 {
     auto data = std::make_pair(T2r, 0.);
-    GSLFunction<decltype(super_lorentzian_integrand), decltype(data)> function(
-        super_lorentzian_integrand, data);
+    gsl_function function{super_lorentzian_integrand, &data};
     auto result = xt::empty<double>(delta_f.shape());
     double abserr;
     for(std::size_t i=0; i < delta_f.size(); ++i)
