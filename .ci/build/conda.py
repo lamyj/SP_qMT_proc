9769927b77eba13c6dff0339d24c7bc5cbfb1145
Julien Lamy <lamy@unistra.fr>
2022-07-24 08:55:08 +0200

    Add macOS CI

--- .ci/build/conda.py
+++ .ci/build/conda.py
@@ -2,5 +2,5 @@ import subprocess
 
 subprocess.check_call([
     "conda", "install", "--yes", "-c", "conda-forge",
-    "cmake", "gsl", "gxx", "make", "nibabel", "numpy", "pybind11",
+    "cmake", "gsl", "cxx-compiler", "make", "nibabel", "numpy", "pybind11",
     "scipy", "xtensor", "xtensor-blas", "xtensor-python"])
