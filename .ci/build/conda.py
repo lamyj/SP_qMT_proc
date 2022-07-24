71c1811b60f87d4e92690e70b0215379e88231a6
Julien Lamy <lamy@unistra.fr>
2022-07-24 19:26:45 +0200

    Fix Windows CI

--- .ci/build/conda.py
+++ .ci/build/conda.py
@@ -2,5 +2,5 @@ import subprocess
 
 subprocess.check_call([
     "conda", "install", "--yes", "-c", "conda-forge",
-    "cmake", "gsl", "cxx-compiler", "make", "nibabel", "numpy", "pybind11",
-    "scipy", "xtensor", "xtensor-blas", "xtensor-python"])
+    "cmake", "gsl", "cxx-compiler", "make", "nibabel", "numpy", "pkg-config",
+    "pybind11", "scipy", "xtensor", "xtensor-blas", "xtensor-python"])
